a955b1041f086d7c439972c57ee71cc0871751e3
Odysseas Trispiotis <oddo1994.ot@gmail.com>
2020-06-02 00:28:22 +0300

    Add deep learning on tests and fix some bugs

--- src/models/FakeNewsDeepLearner.py
+++ src/models/FakeNewsDeepLearner.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.layers import Dense, Dropout, Embedding, GRU, LSTM
 from tensorflow.keras.models import Sequential
-from tensorflow_core.python.keras.wrappers.scikit_learn import KerasRegressor
+from tensorflow.python.keras.wrappers.scikit_learn import KerasClassifier
 
 from src.models.SupervisedLearner import SupervisedLearner
 from src.utils.conf import *
@@ -13,18 +13,22 @@ class FakeNewsDeepLearner(SupervisedLearner):
 
     # TODO: Add optimal parameters from grid search
     def create_learner(self):
+        nn_clf = KerasClassifier(build_fn=self.create_nn_model, verbose=1)
+        return {'clf': nn_clf}
+
+    def create_nn_model(self):
         nn_model = None
 
-        if self.model_name is LSTM:
+        if self.learner_name is LSTM_NN:
             # LSTM Neural Network
             nn_model = Sequential(name='lstm_nn_model')
             nn_model.add(layer=Embedding(input_dim=4500, output_dim=120, name='1st_layer'))
             nn_model.add(layer=LSTM(units=120, dropout=0.2, recurrent_dropout=0.2, name='2nd_layer'))
             nn_model.add(layer=Dropout(rate=0.5, name='3rd_layer'))
-            nn_model.add(layer=Dense(unitsv=120, activation='relu', name='4th_layer'))
+            nn_model.add(layer=Dense(units=120, activation='relu', name='4th_layer'))
             nn_model.add(layer=Dropout(rate=0.5, name='5th_layer'))
             nn_model.add(layer=Dense(units=len(set(self.y_train)), activation='sigmoid', name='output_layer'))
-        elif self.model_name is GRU:
+        elif self.learner_name is GRU_NN:
             # GRU neural Network
             nn_model = Sequential(name='gru_nn_model')
             nn_model.add(layer=Embedding(input_dim=4500, output_dim=120, name='1st_layer'))
@@ -37,9 +41,7 @@ class FakeNewsDeepLearner(SupervisedLearner):
             nn_model.add(layer=Dense(units=len(set(self.y_train)), activation='softmax', name='output_layer'))
 
         nn_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-        learner = KerasRegressor(nn_model, verbose=0)
-
-        return learner
+        return nn_model
 
     def create_default_learner(self):
         pass
--- src/test_models.py
+++ src/test_models.py
@@ -1,25 +1,34 @@
 from src.models.FakeNewsClassifier import FakeNewsClassifier
+from src.models.FakeNewsDeepLearner import FakeNewsDeepLearner
+from src.models.SupervisedLearner import SupervisedLearner
 from src.utils.conf import *
 
 
-def generic_test(clf_names=None, feature_names=None, language=ENGLISH):
+def generic_test(clf_names=None, nn_names=None, feature_names=None, language=ENGLISH):
     metrics_scores = {}
 
     if clf_names is None:
         clf_names = CLASSIFICATION_SET
+
+    if nn_names is None:
+        nn_names = DEEP_LEARNING_SET
+
     if feature_names is None:
         feature_names = FEATURE_SET
 
-    for clf_name in clf_names:
-        for feature_name in feature_names:
+    for feature_name in feature_names:
+        for clf_name in clf_names:
             clf = FakeNewsClassifier(clf_name, feature_name, True, language)
             metrics_scores.update({clf.model_name: clf.metrics})
+        for nn_name in nn_names:
+            nn = FakeNewsDeepLearner(nn_name, feature_name, True, language)
+            metrics_scores.update({nn.model_name: nn.metrics})
 
-    FakeNewsClassifier.plot_roc_curve(metrics_scores)
-    FakeNewsClassifier.save_metrics_to_csv(metrics_scores)
+    SupervisedLearner.plot_roc_curve(metrics_scores)
+    SupervisedLearner.save_metrics_to_csv(metrics_scores)
 
 
 # generic_test(clf_names={RANDOM_FOREST, LOGISTIC_REGRESSION,
 #                         ADA_BOOST, EXTRA_TREES, GAUSSIAN_NB})
 
-generic_test(clf_names={RANDOM_FOREST}, feature_names={W2V}, language=ENGLISH)
+generic_test(clf_names={RANDOM_FOREST}, nn_names={LSTM_NN}, feature_names={BOW}, language=ENGLISH)
